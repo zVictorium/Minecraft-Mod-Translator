31f4d4a491f7b223939372aa1c070e1c2959648f
zVictorium <profreegameyt@gmail.com>
2025-07-06 17:43:35 +0200

    Refactor import statements and adjust project path handling for improved module accessibility

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,8 +1,14 @@
 import sys
+import os
 import traceback
 
+current_dir = os.path.dirname(__file__)
+project_root = os.path.dirname(os.path.dirname(current_dir))  # .../src
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
 try:
-    from .commands.command_line import main
+    from app.commands.command_line import main
     main()
 except Exception as e:
     print(f"Error in Mod Translator: {e}")
